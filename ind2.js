367087ec52b6b146d8a2b745fbd8a28d486ff59d
keland <keland_mans@yahoo.com>
2020-02-14 14:23:50 -0600

    javacw1

--- ind2.js
+++ ind2.js
@@ -1,2 +1,8 @@
-X =prompt( "what is your balance")
-p = ("10- x" = sum);
+
+
+let name1 ="what is your balance";
+
+let p =("10 -x")
+name1 = prompt( "what is your balance");
+p = ("10- x" +sum);
+alert ( "10-x" = sum)
